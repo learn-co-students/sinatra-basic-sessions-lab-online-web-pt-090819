9819d6f8acae9d5b27df06bd70aff20ef8b92342
Alexander Bispo <alexbispo173@gmail.com>
2020-01-05 10:06:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+ 
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+  
+  get '/'
+    erb :index
+  end
+  
+  post "/checkout"
+    @sessions = session
+    item = params["item"]
+    @sessions[:item] = item
+  end
+  
 end
